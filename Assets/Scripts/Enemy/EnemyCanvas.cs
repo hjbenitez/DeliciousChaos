3b56574f72e8c2039ee2ef64ab75298723fb047e
GrayAidan <114709817+GrayAidan@users.noreply.github.com>
2024-05-10 15:45:56 -0400

    Health Bar for enemies

--- Assets/Scripts/Enemy/EnemyCanvas.cs
+++ Assets/Scripts/Enemy/EnemyCanvas.cs
@@ -6,17 +6,8 @@ using UnityEngine.UI;
 public class EnemyCanvas : MonoBehaviour
 {
     public Canvas canvasComp;
-    public GameObject exclimation;
-    public GameObject healthBar;
-    public GameObject damageNumberObjPrefab;
+    public Image healthBar;
     public Transform canvasPos;
-
-    private bool damageTimerTicking;
-    private float timer;
-    private GameObject currentDamageNumObj;
-
-    private int damageNumShown;
-
 
     // Start is called before the first frame update
     void Start()
@@ -24,38 +15,9 @@ public class EnemyCanvas : MonoBehaviour
         
     }
 
-    public void DamageNumber(int damageTaken)
-    {
-        timer = 0;
-        damageTimerTicking = true;
-
-        if(currentDamageNumObj != null)
-        {
-            Destroy(currentDamageNumObj);
-        }
-
-        damageNumShown += damageTaken;
-
-        currentDamageNumObj = Instantiate(damageNumberObjPrefab, canvasComp.transform);
-        currentDamageNumObj.GetComponent<Text>().text = damageNumShown.ToString();
-    }
-
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = new Vector3(canvasPos.position.x, canvasPos.position.y + 0.5f, canvasPos.position.z + 0.9f);
-
-        if (damageTimerTicking && timer < 2)
-        {
-            timer += Time.deltaTime;
-        }
-        else if(damageTimerTicking && timer > 2)
-        {
-            damageNumShown = 0;
-            Destroy(currentDamageNumObj);
-
-            damageTimerTicking = false;
-            timer = 0;
-        }
+        this.transform.position = new Vector3(canvasPos.position.x, canvasPos.position.y + 1.5f, canvasPos.position.z + 0.9f);
     }
 }

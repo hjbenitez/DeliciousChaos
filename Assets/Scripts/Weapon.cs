58f9f424fdb3d94728f6fbb9839ff7f1a59f121f
Humberto Benitez <humbertobenitez5760@yahoo.ca>
2024-05-11 17:02:36 -0400

    Weapon mesh switching, weapon class created

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+    public abstract Transform GetNozzle();
+    public abstract Projectile GetProjectile();
+
+    public abstract float GetFireRate();
+}

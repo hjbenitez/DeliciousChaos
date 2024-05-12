c4ec622c5c7d3045c6098a68358492425a3aeabc
Humberto Benitez <humbertobenitez5760@yahoo.ca>
2024-05-12 14:59:05 -0400

    cake splatter

--- Assets/Scripts/Fork.cs
+++ Assets/Scripts/Fork.cs
@@ -8,6 +8,7 @@ public class Fork : Projectile
     public float lifeTime = 5f;
     public int damage = 1;
     public float fireRate = 0.25f;
+    public ParticleSystem cakeSplatter;
 
     Rigidbody rb;
     float lifeTimer = 0f;
@@ -47,6 +48,7 @@ public class Fork : Projectile
             if(other.gameObject.tag == "Enemy")
             {
                 other.gameObject.GetComponent<EnemyController>().TakeDamage(damage);
+                Instantiate(cakeSplatter, transform.position, transform.rotation);
             }
 
             Destroy(gameObject);

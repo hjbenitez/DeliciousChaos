5cde1fc3426d9c7dba71b7df49fa3ca8b527aa18
Humberto Benitez <humbertobenitez5760@yahoo.ca>
2024-05-12 23:23:15 -0400

    Added menu music

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.UI;
 using UnityEngine;
 using static Unity.VisualScripting.Member;
 using static UnityEngine.GraphicsBuffer;
